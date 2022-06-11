5278adb000ef15a744344f2f6475ef005ca12769
celsofisica <celso.fisica@gmail.com>
2022-06-11 13:57:44 -0300

    Mudança de Sprite

--- Assets/Code/Game_Cntrl.cs
+++ Assets/Code/Game_Cntrl.cs
@@ -5,31 +5,29 @@ using UnityEngine;
 public class Game_Cntrl : MonoBehaviour
 {
     GameObject Card;
-    SpriteRenderer sprite;
+    int nCard = 0;
+    public SpriteRenderer sprite;
+    Sprite A_H;
 
-    public string[] carta = new string[]
-{
-        "A_H","K_H","Q_H","J_H","K_H",
-        "A_S","K_S","Q_S","J_S","K_S",
-        "A_D","K_D","Q_D","J_D","K_D",
-        "A_C","K_C","Q_C","J_C","K_C",
-        "Back",
-};
+    public Sprite[] carta;
+//carta[] 
+//{
+//       A_H,
+//        //"K_H","Q_H","J_H","K_H",
+//        //"A_S","K_S","Q_S","J_S","K_S",
+//        //"A_D","K_D","Q_D","J_D","K_D",
+//        //"A_C","K_C","Q_C","J_C","K_C",
+//        //"Back",
+//};
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(carta[1]);
-        sprite = GameObject.Find("Card").GetComponent<SpriteRenderer>();
 
-        sprite.name = carta[1];
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            nCard = (nCard + 1) % carta.Length;
+            sprite.sprite = carta[nCard];
+        }
 
     }
 }
