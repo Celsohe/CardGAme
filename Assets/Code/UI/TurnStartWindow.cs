2a829bbcee1d2e046823bf8e5d4d8977bb6b6eef
Gabriel Coutinho <gabrielcac@gmail.com>
2022-08-24 20:01:22 -0300

    Tela para cobrir o jogo durante a troca de turno

---
+++ Assets/Code/UI/TurnStartWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Code.Game;
+using Code.Turn;
+using UnityEngine;
+
+namespace Code.UI
+{
+	public sealed class TurnStartWindow : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject _content;
+		
+		private void Awake()
+		{
+			TurnController.OnTurnChanged += OnTurnChanged;
+		}
+
+		private void OnDestroy()
+		{
+			TurnController.OnTurnChanged -= OnTurnChanged;
+		}
+
+		private void OnTurnChanged(Player.Index turn)
+		{
+			_content.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Code/UI/TurnStartWindowLabel.cs
@@ -0,0 +1,30 @@
+﻿using Code.Game;
+using Code.Turn;
+using TMPro;
+using UnityEngine;
+
+namespace Code.UI
+{
+	public sealed class TurnStartWindowLabel : MonoBehaviour
+	{
+		[SerializeField]
+		private string _textFormat = "Vez do jogador {0}"; 
+		[SerializeField]
+		private TMP_Text _textField;
+		
+		private void Awake()
+		{
+			TurnController.OnTurnChanged += OnTurnChanged;
+		}
+
+		private void OnDestroy()
+		{
+			TurnController.OnTurnChanged -= OnTurnChanged;
+		}
+
+		private void OnTurnChanged(Player.Index turn)
+		{
+			_textField.text = string.Format(_textFormat, (int)turn);
+		}
+	}
+}
