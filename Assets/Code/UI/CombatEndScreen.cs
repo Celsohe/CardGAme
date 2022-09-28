ccb021adf98b0b89aeda1d0d5e66e2381958a059
Gabriel Coutinho <gabrielcac@gmail.com>
2022-09-28 19:41:38 -0300

    Bloquei de input durante a troca de turno após o fim do combate

--- Assets/Code/UI/CombatEndScreen.cs
+++ Assets/Code/UI/CombatEndScreen.cs
@@ -11,6 +11,8 @@ namespace Code.UI
 		[SerializeField]
 		private GameObject _content;
 		[SerializeField]
+		private GameObject _inputBlocker;
+		[SerializeField]
 		private string _textFormat = "Jogador {0} venceu esse combate"; 
 		[SerializeField]
 		private TMP_Text _textField;
@@ -18,22 +20,26 @@ namespace Code.UI
 		private void Awake()
 		{
 			_content.SetActive(false);
+			_inputBlocker.SetActive(false);
 		}
 		
 		private void OnEnable()
 		{
 			CardPileFace.OnCardPileCombat += OnCardPileCombat;
 			TurnController.OnTurnChanged += OnTurnChanged;
+			TableTurner.OnTableTurned += OnTableTurned;
 		}
 
 		private void OnDisable()
 		{
 			CardPileFace.OnCardPileCombat -= OnCardPileCombat;
 			TurnController.OnTurnChanged -= OnTurnChanged;
+			TableTurner.OnTableTurned -= OnTableTurned;
 		}
 
 		private void OnCardPileCombat(Player.Index winner)
 		{
+			_inputBlocker.SetActive(true);
 			_content.SetActive(true);
 
 			_textField.text = string.Format(_textFormat, (int)winner);
@@ -42,6 +48,11 @@ namespace Code.UI
 		private void OnTurnChanged(Player.Index player)
 		{
 			_content.SetActive(false);
+		}
+
+		private void OnTableTurned()
+		{
+			_inputBlocker.SetActive(false);
 		}
 	}
 }
